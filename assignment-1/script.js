b08c2b57914a3d127df834c783e8d4a8f380eb2e
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2024-01-17 11:11:17 -0600

    Add assignment 1

---
+++ assignment-1/script.js
@@ -0,0 +1,13 @@
+/*
+Name: Luke Hamann
+Date: 2024-01-21
+*/
+"use strict";
+
+let myNumber = 14;
+myNumber /= 2;
+
+let favoriteThing = prompt("What's your favorite thing?");
+
+console.log("myNumber: " + myNumber);
+console.log("favoriteThing: " + favoriteThing);
