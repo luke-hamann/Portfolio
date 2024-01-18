17cd3956783d48b731573e6b853a6fcce277d0e0
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2024-01-18 12:11:47 -0600

    Update assignment 2 script

--- assignment-2/script.js
+++ assignment-2/script.js
@@ -4,16 +4,11 @@ Date: 2024-01-21
 */
 "use strict";
 
-function myFunc(value) {
-    if (value % 2 != 0) {
-        console.log(value);
-    }
-    value++;
-    return value;
-}
-
 let myNumber = 3;
 
 for (let i = 0; i < 5; i++) {
-    myNumber = myFunc(myNumber);
+    if (myNumber % 2 != 0) {
+        console.log(myNumber);
+    }
+    myNumber++;
 }
