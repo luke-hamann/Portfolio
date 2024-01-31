ea7957ae036f7f4d67a8e175efbf3ac58364aa51
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2024-01-31 11:07:50 -0600

    Move script element to head

--- assignment-4/dom.js
+++ assignment-4/dom.js
@@ -2,17 +2,19 @@
 
 const $ = selector => document.querySelector(selector);
 
-let container = $("div");
+document.addEventListener("DOMContentLoaded", () => {
+    let container = $("div");
 
-for (let i = 0; i < 5; i++) {
-    let newParagraph = document.createElement("p");
-    newParagraph.textContent = "Hello, world!";
-    container.appendChild(newParagraph);
-}
-
-function changeColors() {
-    document.body.classList.toggle("pink");
-    container.classList.toggle("blue");
-}
-
-$("button").onclick = changeColors;
+    for (let i = 0; i < 5; i++) {
+        let newParagraph = document.createElement("p");
+        newParagraph.textContent = "Hello, world!";
+        container.appendChild(newParagraph);
+    }
+    
+    function changeColors() {
+        document.body.classList.toggle("pink");
+        container.classList.toggle("blue");
+    }
+    
+    $("button").onclick = changeColors;
+});
